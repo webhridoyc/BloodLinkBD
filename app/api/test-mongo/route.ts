3c8aa57580bd6ea754eefc3d477869229f468d93
Hridoy Chondro <work.hridoy.chondro@gmail.com>
2025-05-29 06:32:09 +0000

    Remove test-mongo route

--- app/api/test-mongo/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  try {
- return NextResponse.json({ message: "MongoDB functionality removed" });
-  }
-}
